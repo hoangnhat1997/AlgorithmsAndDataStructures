a54f61b2f688e20085bc4bb09763d3e986793b74
NhatPham <nhatpham@192.168.2.18>
2023-07-03 15:50:09 +0700

    Matrix

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,36 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked =[];
+  let index =0;
+  while(index< array.length){
+    chunked.push(array.slice(index,index+size));
+    index += size;
+  }
+  return chunked;
+}
+
+chunk([1, 2, 3, 4, 5, 6, 7, 8], 3)
 
 module.exports = chunk;
+
+// Solution 1
+//   const chunked = [];
+
+//     for( let element of array){
+//         const last = chunked[chunked.length -1];
+//         console.log('--element--',element);
+//         console.log('--chunked--',chunked);
+//         console.log('--last--',last);
+
+        
+//         if(!last || last.length ===size){
+//             console.log("==if===");
+//             chunked.push([element]);
+//         }else{
+//             console.log("==else===");
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    debugger;
+    return str.split('').reduce((reversed, character)=>
+         character + reversed
+    , '');
+
+}
+
+reverse('1234das');
 
 module.exports = reverse;
+
+// ** Solution 1
+// function reverse(str) {
+//     const  arr = str.split('');
+//     arr.reverse();
+//    return arr.join('');
+// }
+
+// ** Solution 2
+// function reverse(str) {
+//     let reversed = '';
+//     for(let character of str){
+//         reversed = character + reversed;
+//     }
+//     return reversed;
+// }
+
+// ** Solution 3
+// function reverse(str) {
+//     debugger;
+//     return str.split('').reduce((reversed, character)=>
+//          character + reversed
+//     , '');
+// }
