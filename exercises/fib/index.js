d1c4d0ba619d70cab7fcf44d4c5904368ffe2df4
NhatPham <nhatpham@192.168.2.18>
2023-07-21 15:50:52 +0700

    update Fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,12 +8,27 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
+function memorize(fn) {
+  const cache = {};
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memorize(slowFib);
 
 module.exports = fib;
 
