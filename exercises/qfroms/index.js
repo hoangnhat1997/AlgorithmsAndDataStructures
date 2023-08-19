fb3d9f3a2924458b0dac7ca5549a4a100090ccec
NhatPham <nhatpham@192.168.2.18>
2023-08-19 22:03:47 +0700

    qfroms2

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -22,6 +22,34 @@ class Queue {
   add(record) {
     this.first.push(record);
   }
+
+  remove() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.pop();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
+
+  peek() {
+    while (this.first.peek()) {
+      this.second.push(this.first.pop());
+    }
+
+    const record = this.second.peek();
+
+    while (this.second.peek()) {
+      this.first.push(this.second.pop());
+    }
+
+    return record;
+  }
 }
 
 module.exports = Queue;
