a9b93d08f5476cdf2651af68b68eaa015819aff3
Pham Vuong Hoang Nhat <phamvuonghoangnhat@gmail.com>
2024-09-08 23:06:49 +0700

    add LinkedList solution

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -16,6 +16,21 @@ class LinkedList {
   insertFirst(data) {
     this.head = new Node(data, this.head);
   }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+
+    return counter;
+  }
+  getFirst() {
+    return this.head;
+  }
 }
 
 module.exports = { Node, LinkedList };
